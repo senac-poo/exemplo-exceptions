8a129d2fe19040099e94432d4f6ceaff0dfbc8ce
Celso Crivelaro <celso.crivelaro@revelo.com.br>
2020-04-18 14:17:47 -0300

    Correção de bugs

--- src/banco/Factories/ContaFactory.java
+++ src/banco/Factories/ContaFactory.java
@@ -13,13 +13,13 @@ public class ContaFactory {
 	public Conta criarConta(int tipoConta) {
 		if(tipoConta == 1) {
 			System.out.println("Criando Conta Corrente...");
-			return new ContaCorrente(AGENCIA, ultimaConta++);
+			return new ContaCorrente(AGENCIA, ++ultimaConta);
 		} else if(tipoConta == 2) {
 			System.out.println("Criando Conta Poupança...");
-			 return new ContaPoupanca(AGENCIA, ultimaConta++);
+			 return new ContaPoupanca(AGENCIA, ++ultimaConta);
 		} else if(tipoConta == 3) {
 			System.out.println("Criando Conta Poupança Especial...");
-			return new ContaPoupancaEspecial(AGENCIA, ultimaConta++);
+			return new ContaPoupancaEspecial(AGENCIA, ++ultimaConta);
 		} else {
 			System.out.println("Opção inválida!");
 			return null;
